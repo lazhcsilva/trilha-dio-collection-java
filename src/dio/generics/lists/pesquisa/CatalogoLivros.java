bf5cacc7d3084962e045c664a0d5c49fa50fc720
Lázaro Silva <lazhcsilva@gmail.com>
2024-05-03 10:00:42 -0300

    Atualizando Pesquisa de Livros

--- src/dio/generics/lists/pesquisa/CatalogoLivros.java
+++ src/dio/generics/lists/pesquisa/CatalogoLivros.java
@@ -51,4 +51,17 @@ public class CatalogoLivros {
         }
         return livroPorTitulo;
     }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisaPorIntervaloAnos(2020, 2021));
+        System.out.println(catalogoLivros.pesquisaPorTitulo("Livro 1"));
+    }
 }
