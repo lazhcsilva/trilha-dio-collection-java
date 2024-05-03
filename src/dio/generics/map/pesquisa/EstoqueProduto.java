22c35de1f51849039d75fbf1a3962914b0ce8c9c
Lázaro Silva <lazhcsilva@gmail.com>
2024-05-03 15:25:54 -0300

    Utilizando interface Map para pesquisa

---
+++ src/dio/generics/map/pesquisa/EstoqueProduto.java
@@ -0,0 +1,59 @@
+package dio.generics.map.pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstoqueProduto {
+
+    private Map<Long, Produto> estoqueProdutoMap;
+
+    public EstoqueProduto() {
+        this.estoqueProdutoMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutoMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos() {
+        System.out.println(estoqueProdutoMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        Double valorTotalEstoque = 0d;
+        if(!estoqueProdutoMap.isEmpty()) {
+            for (Produto p : estoqueProdutoMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutoMap.isEmpty()) {
+            for(Produto p : estoqueProdutoMap.values()) {
+                if(p.getPreco() > maiorPreco) {
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProduto estoque = new EstoqueProduto();
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Produto A", 10, 5.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 10.0);
+        estoque.adicionarProduto(3L, "Produto C", 2, 15.0);
+        estoque.adicionarProduto(4L, "Produto D", 2, 20.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+    }
+}
